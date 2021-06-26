a107e8c826b5734771fcb57ee36ff0c255771479
Dan Miller <dsmiller95@gmail.com>
2021-06-26 10:48:13 -0500

    add new generic binary serializing wrapper

---
+++ Assets/UtilityScripts/com.dman.utilities/Runtime/SerializableUnityObjects/BinarySerialized.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+namespace Dman.Utilities.SerializableUnityObjects
+{
+    [Serializable]
+    public class BinarySerialized<T> : ISerializationCallbackReceiver where T : class, new()
+    {
+        public T data;
+        [SerializeField]
+        private byte[] allFileData;
+
+        public BinarySerialized()
+        {
+            this.data = new T();
+        }
+        public BinarySerialized(T backing)
+        {
+            this.data = backing;
+        }
+        public void OnAfterDeserialize()
+        {
+            if (allFileData == null)
+            {
+                data = null;
+                return;
+            }
+            using (MemoryStream stream = new MemoryStream(allFileData))
+            {
+                var resultObject = new BinaryFormatter().Deserialize(stream);
+                data = resultObject as T;
+            }
+
+        }
+
+        public void OnBeforeSerialize()
+        {
+            if (data == null)
+            {
+                allFileData = null;
+                return;
+            }
+            using (MemoryStream stream = new MemoryStream())
+            {
+                new BinaryFormatter().Serialize(stream, data);
+                this.allFileData = stream.ToArray();
+            }
+        }
+    }
+}
