65df5e52e74464667bd8606f6c0811bb18e219ef
Dan Miller <dsmiller95@gmail.com>
2021-07-12 21:49:38 -0500

    add a transform saver utility. bump to 0.1.3

---
+++ Assets/UtilityScripts/com.dman.scene-save-system/Runtime/SharedSaveables/TransformSaver.cs
@@ -0,0 +1,52 @@
+﻿using Dman.Utilities.SerializableUnityObjects;
+using System;
+using UnityEngine;
+
+namespace Dman.SceneSaveSystem.SharedSaveables
+{
+    public class TransformSaver : MonoBehaviour, ISaveableData
+    {
+        [Serializable]
+        class TransformSaveObject
+        {
+            private SerializableVector3 position;
+            private SerializableVector3 eulerAngles;
+            private SerializableVector3 scale;
+
+            public TransformSaveObject(Transform source)
+            {
+                position = source.position;
+                eulerAngles = source.eulerAngles;
+                scale = source.localScale;
+            }
+
+            public void Apply(Transform target)
+            {
+                target.position = position;
+                target.eulerAngles = eulerAngles;
+                target.localScale = scale;
+            }
+        }
+
+        public string uniqueNameInsideContext = "Something Unique";
+
+        public string UniqueSaveIdentifier => "transform data: " + uniqueNameInsideContext;
+
+        public object GetSaveObject()
+        {
+            return new TransformSaveObject(transform);
+        }
+
+        public void SetupFromSaveObject(object save)
+        {
+            if (save is TransformSaveObject savedData)
+            {
+                savedData.Apply(transform);
+            }
+        }
+        public ISaveableData[] GetDependencies()
+        {
+            return new ISaveableData[0];
+        }
+    }
+}
