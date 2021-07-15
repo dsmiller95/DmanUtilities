86694bfe3ce5b90a7864c0aee1050ca75664b322
Dan Miller <dsmiller95@gmail.com>
2021-07-15 12:52:43 -0500

    upgrade the boolean change listener. no longer only one edge, for convenience

---
+++ Assets/UtilityScripts/com.dman.reactive-variables/Runtime/VariableOperators/BooleanChangeToUnityEvent.cs
@@ -0,0 +1,33 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Dman.ReactiveVariables.VariableOperators
+{
+    public class BooleanChangeToUnityEvent : MonoBehaviour
+    {
+        public BooleanVariable booleanToWatch;
+
+        public UnityEvent OnChangedToTrue;
+        public UnityEvent OnChangedToFalse;
+
+        private void Awake()
+        {
+            booleanToWatch.Value.TakeUntilDestroy(this)
+                .Pairwise()
+                .Subscribe(pair =>
+                {
+                    if (pair.Current != pair.Previous)
+                    {
+                        if (pair.Current)
+                        {
+                            OnChangedToTrue?.Invoke();
+                        }else
+                        {
+                            OnChangedToFalse.Invoke();
+                        }
+                    }
+                }).AddTo(this);
+        }
+    }
+}
