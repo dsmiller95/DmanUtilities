0f8fa2c29ab2fe3fc0dae36ee3ae54ee42c4a5be
Dan Miller <dsmiller95@gmail.com>
2021-08-14 17:16:29 -0500

    add time scale binding

---
+++ Assets/UtilityScripts/com.dman.reactive-variables/Runtime/Bindings/TimeScaleBinding.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+using UniRx;
+namespace Dman.ReactiveVariables.Bindings
+{
+    public class TimeScaleBinding : MonoBehaviour
+    {
+        public FloatReference floatVariable;
+        public float speedMultiplier = 1;
+        private void Awake()
+        {
+            floatVariable.ValueChanges.TakeUntilDestroy(this)
+                .Subscribe(next =>
+                {
+                    Time.timeScale = next * speedMultiplier;
+                }).AddTo(this);
+        }
+    }
+}
