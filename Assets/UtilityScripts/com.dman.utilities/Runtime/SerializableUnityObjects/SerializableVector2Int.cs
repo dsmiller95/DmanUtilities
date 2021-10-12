3e3b5cbf02c58ca044699a4de9f1bd8c9512aa7b
Dan Miller <dsmiller95@gmail.com>
2021-10-12 09:11:07 -0500

    add int vectors to serializables

---
+++ Assets/UtilityScripts/com.dman.utilities/Runtime/SerializableUnityObjects/SerializableVector2Int.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace Dman.Utilities.SerializableUnityObjects
+{
+    [Serializable]
+    public class SerializableVector2Int
+    {
+        private int[] vectorData;
+        public SerializableVector2Int(Vector2Int vector)
+        {
+            vectorData = new int[] {
+                vector.x,
+                vector.y
+                };
+        }
+
+        public Vector2Int GetDeserialized()
+        {
+            var newVector = new Vector2Int();
+            newVector.x = vectorData[0];
+            newVector.y = vectorData[1];
+            return newVector;
+        }
+
+        public static implicit operator Vector2Int(SerializableVector2Int serializable) => serializable.GetDeserialized();
+        public static implicit operator SerializableVector2Int(Vector2Int notSerializable) => new SerializableVector2Int(notSerializable);
+    }
+}
---
+++ Assets/UtilityScripts/com.dman.utilities/Runtime/SerializableUnityObjects/SerializableVector3Int.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace Dman.Utilities.SerializableUnityObjects
+{
+    [Serializable]
+    public class SerializableVector3Int
+    {
+        private int[] vectorData;
+        public SerializableVector3Int(Vector3Int vector)
+        {
+            vectorData = new int[] {
+                vector.x,
+                vector.y,
+                vector.z
+                };
+        }
+
+        public Vector3Int GetDeserialized()
+        {
+            var newVector = new Vector3Int();
+            newVector.x = vectorData[0];
+            newVector.y = vectorData[1];
+            newVector.z = vectorData[2];
+            return newVector;
+        }
+
+        public static implicit operator Vector3Int(SerializableVector3Int serializable) => serializable.GetDeserialized();
+        public static implicit operator SerializableVector3Int(Vector3Int notSerializable) => new SerializableVector3Int(notSerializable);
+    }
+}
