142cb5c81db17c162dd9dacd7988bc79ff4ff98e
Dan Miller <dsmiller95@gmail.com>
2021-10-12 09:10:32 -0500

    add float variable incrementor for reactive variables

---
+++ Assets/UtilityScripts/com.dman.reactive-variables/Runtime/VariableOperators/FloatVariableIncrementor.cs
@@ -0,0 +1,18 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Dman.ReactiveVariables.VariableOperators
+{
+    public class FloatVariableIncrementor : MonoBehaviour
+    {
+        public FloatVariable variable;
+        public float incrementPerSecond;
+
+        private void Update()
+        {
+            variable.Add(Time.deltaTime * incrementPerSecond);
+        }
+    }
+}
