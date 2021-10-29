7abdf1260d663ead823ca5299a8ea7f3ba94cbe3
Dan Miller <dsmiller95@gmail.com>
2021-10-29 16:15:23 -0500

    add a utility component to hold on to a scene reference, and allow an event binding to switch to it while preserving save state

---
+++ Assets/UtilityScripts/com.dman.scene-save-system/Runtime/Components/SwitchScene.cs
@@ -0,0 +1,22 @@
+﻿using Dman.Utilities;
+using UnityEngine;
+
+namespace Dman.SceneSaveSystem
+{
+    /// <summary>
+    /// used to switch the current scene to another scene
+    ///     preserving current state and loading new state
+    /// </summary>
+    public class SwitchScene : MonoBehaviour
+    {
+        public SceneReference targetScene;
+        public WorldSaveManager saveManager;
+
+        public void SwitchToTargetScene()
+        {
+            saveManager.SaveActiveScene();
+
+            saveManager.Load(targetScene.scenePath);
+        }
+    }
+}
