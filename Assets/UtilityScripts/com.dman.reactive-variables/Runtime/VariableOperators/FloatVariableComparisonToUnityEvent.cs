38b5f616e0c61008c74f2fe6d98180ba438e0e2f
Dan Miller <dsmiller95@gmail.com>
2022-01-11 14:40:08 -0600

    add new reusable reactive variable components

---
+++ Assets/UtilityScripts/com.dman.reactive-variables/Runtime/VariableOperators/FloatVariableComparisonToUnityEvent.cs
@@ -0,0 +1,39 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Dman.ReactiveVariables.VariableOperators
+{
+    public class FloatVariableComparisonToUnityEvent : MonoBehaviour
+    {
+        public FloatReference valueA;
+        public FloatReference valueB;
+
+        public UnityEvent OnAGreaterThanB;
+        public UnityEvent OnALessThanB;
+
+        private bool lastComparisonResult;
+
+        private void Awake()
+        {
+            valueA.ValueChanges.TakeUntilDestroy(this)
+                .Merge(valueB.ValueChanges)
+                .StartWith(0)
+                .Subscribe(valueUpdate =>
+                {
+                    var AGreaterThanB = valueA.CurrentValue > valueB.CurrentValue;
+                    if (lastComparisonResult != AGreaterThanB) {
+                        if (AGreaterThanB)
+                        {
+                            OnAGreaterThanB.Invoke();
+                        }else
+                        {
+                            OnALessThanB.Invoke();
+                        }
+                    }
+                    lastComparisonResult = AGreaterThanB;
+                }).AddTo(this);
+        }
+
+    }
+}
---
+++ Assets/UtilityScripts/com.dman.reactive-variables/Runtime/VariableOperators/IntVariableComparisonToUnityEvent.cs
@@ -0,0 +1,62 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Dman.ReactiveVariables.VariableOperators
+{
+    public class IntVariableComparisonToUnityEvent : MonoBehaviour
+    {
+        public IntReference valueA;
+        public IntReference valueB;
+
+        public UnityEvent OnAGreaterThanB;
+        public UnityEvent OnAEqualsB;
+        public UnityEvent OnALessThanB;
+
+        private int lastComparisonResult;
+
+        private void Awake()
+        {
+            valueA.ValueChanges.TakeUntilDestroy(this)
+                .Merge(valueB.ValueChanges)
+                .StartWith(0)
+                .Subscribe(valueUpdate =>
+                {
+                    var comparisonResult = -1;
+                    if (valueA.CurrentValue > valueB.CurrentValue)
+                    {
+                        comparisonResult = 0;
+                    }
+                    else if (valueA.CurrentValue >= valueB.CurrentValue)
+                    {
+                        comparisonResult = 1;
+                    }
+                    else
+                    {
+                        comparisonResult = 2;
+                    }
+
+                    if(comparisonResult == lastComparisonResult)
+                    {
+                        return;
+                    }
+                    lastComparisonResult = comparisonResult;
+                    switch (comparisonResult)
+                    {
+                        case 0:
+                            OnAGreaterThanB.Invoke();
+                            break;
+                        case 1:
+                            OnAEqualsB.Invoke();
+                            break;
+                        case 2:
+                            OnALessThanB.Invoke();
+                            break;
+                        default:
+                            break;
+                    }
+                }).AddTo(this);
+        }
+
+    }
+}
