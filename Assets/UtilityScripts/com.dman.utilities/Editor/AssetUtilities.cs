bb116db95d62ea32849654c8fb676be086ce35dd
Dan Miller <dsmiller95@gmail.com>
2022-01-15 16:32:19 -0600

    add asset utility methods

---
+++ Assets/UtilityScripts/com.dman.utilities/Editor/AssetUtilities.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Dman.Utilities
+{
+    public static class AssetUtilities
+    {
+        public static ScriptableObject[] GetAllInstances(Type instanceType)
+        {
+            if (!typeof(ScriptableObject).IsAssignableFrom(instanceType))
+            {
+                return null;
+            }
+            var guids = AssetDatabase.FindAssets("t:" + instanceType.Name);  //FindAssets uses tags check documentation for more info
+            var a = new ScriptableObject[guids.Length];
+            for (int i = 0; i < guids.Length; i++)
+            {
+                var path = AssetDatabase.GUIDToAssetPath(guids[i]);
+                a[i] = AssetDatabase.LoadAssetAtPath<ScriptableObject>(path);
+            }
+
+            return a;
+        }
+        public static T[] GetAllInstances<T>() where T : ScriptableObject
+        {
+            string[] guids = AssetDatabase.FindAssets("t:" + typeof(T).Name);  //FindAssets uses tags check documentation for more info
+            T[] a = new T[guids.Length];
+            for (int i = 0; i < guids.Length; i++)
+            {
+                string path = AssetDatabase.GUIDToAssetPath(guids[i]);
+                a[i] = AssetDatabase.LoadAssetAtPath<T>(path);
+            }
+
+            return a;
+        }
+    }
+}
