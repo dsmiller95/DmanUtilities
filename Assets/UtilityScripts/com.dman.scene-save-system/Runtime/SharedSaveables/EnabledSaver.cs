7f449e74c8ae36ac1bd22633361d65763401850c
Dan Miller <dsmiller95@gmail.com>
2022-03-09 10:06:17 -0600

    add a new component to save if a gameobject is active or not

---
+++ Assets/UtilityScripts/com.dman.scene-save-system/Runtime/SharedSaveables/EnabledSaver.cs
@@ -0,0 +1,44 @@
+﻿using Dman.Utilities.SerializableUnityObjects;
+using System;
+using UnityEngine;
+
+namespace Dman.SceneSaveSystem.SharedSaveables
+{
+    public class EnabledSaver : MonoBehaviour, ISaveableData
+    {
+        [Serializable]
+        class SaveObject
+        {
+            private bool isActive;
+
+            public SaveObject(GameObject source)
+            {
+                isActive = source.activeInHierarchy;
+            }
+
+            public void Apply(GameObject target)
+            {
+                target.SetActive(isActive);
+            }
+        }
+
+        public string uniqueNameInsideContext = "Something Unique";
+
+        public string UniqueSaveIdentifier => "gameobject is active: " + uniqueNameInsideContext;
+
+        public int LoadOrderPriority => 0;
+
+        public object GetSaveObject()
+        {
+            return new SaveObject(gameObject);
+        }
+
+        public void SetupFromSaveObject(object save)
+        {
+            if (save is SaveObject savedData)
+            {
+                savedData.Apply(gameObject);
+            }
+        }
+    }
+}
