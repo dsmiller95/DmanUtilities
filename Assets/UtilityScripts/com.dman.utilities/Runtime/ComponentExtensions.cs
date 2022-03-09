24411e71c6e4a0c921bf69a4761e9a9ad9df7180
Dan Miller <dsmiller95@gmail.com>
2022-03-09 10:05:30 -0600

    update destroy all children code to not necessitate using destroyImmediate

--- Assets/UtilityScripts/com.dman.utilities/Runtime/ComponentExtensions.cs
+++ Assets/UtilityScripts/com.dman.utilities/Runtime/ComponentExtensions.cs
@@ -4,21 +4,36 @@ namespace Dman.Utilities
 {
     public static class ComponentExtensions
     {
-        public static void DestroyAllChildren(this GameObject gameObject)
+        public static void DestroyAllChildren(this GameObject gameObject, bool destroyImmediate)
         {
-            for (int i = gameObject.transform.childCount; i > 0; --i)
+            for (int i = gameObject.transform.childCount - 1; i >= 0; --i)
             {
-                Object.DestroyImmediate(gameObject.transform.GetChild(0).gameObject);
-            }
-        }
-        public static void DestroyAllChildren(this GameObject gameObject, System.Func<GameObject, bool> predicate)
-        {
-            for (int i = gameObject.transform.childCount; i > 0; --i)
-            {
-                var child = gameObject.transform.GetChild(0).gameObject;
-                if (predicate(child))
+                var child = gameObject.transform.GetChild(i).gameObject;
+                if (destroyImmediate)
                 {
                     Object.DestroyImmediate(child);
+                }else
+                {
+                    Object.Destroy(child);
+                }
+            }
+        }
+        public static void DestroyAllChildren(this GameObject gameObject, System.Func<GameObject, bool> predicate, bool destroyImmediate)
+        {
+            for (int i = gameObject.transform.childCount - 1; i >= 0; --i)
+            {
+                var child = gameObject.transform.GetChild(i).gameObject;
+                if (!predicate(child))
+                {
+                    continue;
+                }
+                if (destroyImmediate)
+                {
+                    Object.DestroyImmediate(child);
+                }
+                else
+                {
+                    Object.Destroy(child);
                 }
             }
         }
