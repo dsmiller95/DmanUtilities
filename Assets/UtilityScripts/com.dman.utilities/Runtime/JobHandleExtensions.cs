fb8e3826f494750118548fa7ba02c4c88a19b5bd
Dan Miller <dsmiller95@gmail.com>
2022-03-29 10:16:06 -0500

    add utility methods for unitask

---
+++ Assets/UtilityScripts/com.dman.utilities/Runtime/JobHandleExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Unity.Jobs;
+using Cysharp.Threading.Tasks;
+using System.Threading;
+
+namespace Dman.Utilities
+{
+    /// <summary>
+    /// Utility to help with locking of resources which can be read in parallel by multiple jobs,
+    ///     but still need to be edited occasionally
+    /// </summary>
+    public static class JobHandleExtensions
+    {
+        /// <summary>
+        /// Await on a job handle to complete, without forcing completion unless cancelled
+        /// </summary>
+        /// <param name="currentJobHandle"></param>
+        /// <param name="cancel"></param>
+        /// <returns>true if cancelled</returns>
+        public static async UniTask<bool> ToUniTaskImmediateCompleteOnCancel(this JobHandle currentJobHandle, CancellationToken cancel)
+        {
+            var cancelled = false;
+            while (!currentJobHandle.IsCompleted && !cancel.IsCancellationRequested && !cancelled)
+            {
+                var (cancelledTask, registration) = cancel.ToUniTask();
+                var completedIndex = await UniTask.WhenAny(
+                    cancelledTask,
+                    UniTask.Yield(PlayerLoopTiming.PreUpdate, cancel).SuppressCancellationThrow(),
+                    UniTask.Yield(PlayerLoopTiming.Update, cancel).SuppressCancellationThrow(),
+                    UniTask.Yield(PlayerLoopTiming.PostLateUpdate, cancel).SuppressCancellationThrow()
+                    );
+                cancelled = completedIndex == 0;
+                registration.Dispose();
+            }
+            currentJobHandle.Complete();
+            return cancelled || cancel.IsCancellationRequested;
+        }
+    }
+}
---
+++ Assets/UtilityScripts/com.dman.utilities/Runtime/TaskUtil.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Unity.Jobs;
+using Cysharp.Threading.Tasks;
+using System.Threading;
+
+namespace Dman.Utilities
+{
+    /// <summary>
+    /// Utility to help with locking of resources which can be read in parallel by multiple jobs,
+    ///     but still need to be edited occasionally
+    /// </summary>
+    public static class TaskUtil
+    {
+        /// <summary>
+        /// Await on a job handle to complete, without forcing completion unless cancelled
+        /// </summary>
+        /// <param name="currentJobHandle"></param>
+        /// <param name="cancel"></param>
+        /// <returns>true if cancelled</returns>
+        public static CancellationToken RefreshToken(ref CancellationTokenSource source)
+        {
+            if(source != null)
+            {
+                source.Cancel();
+                source.Dispose();
+            }
+            source = new CancellationTokenSource();
+
+            return source.Token;
+        }
+    }
+}
