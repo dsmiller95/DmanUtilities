fbce9acadf32a912ff3d299e355cabb8d0bc66aa
Dan Miller <dsmiller95@gmail.com>
2022-03-31 09:43:06 -0500

    add extension to decompose a matrix into TRS tuple

---
+++ Assets/UtilityScripts/com.dman.utilities/Runtime/OrientationExtensions.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Dman.Utilities
+{
+    public static class OrientationExtensions
+    {
+        public static (Vector3 position, Quaternion rotation, Vector3 scale) DecompseTRS(this Matrix4x4 matrix)
+        {
+            // Extract new local position
+            Vector3 position = matrix.GetColumn(3);
+
+            // Extract new local rotation
+            Quaternion rotation = Quaternion.LookRotation(
+                matrix.GetColumn(2),
+                matrix.GetColumn(1)
+            );
+
+            // Extract new local scale
+            Vector3 scale = new Vector3(
+                matrix.GetColumn(0).magnitude,
+                matrix.GetColumn(1).magnitude,
+                matrix.GetColumn(2).magnitude
+            );
+
+            return (position, rotation, scale);
+        }
+    }
+}
