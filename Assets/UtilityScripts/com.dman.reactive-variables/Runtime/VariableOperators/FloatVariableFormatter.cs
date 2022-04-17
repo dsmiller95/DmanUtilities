398c983f77e0bdad119002b761e3b3d9805e9451
Dan Miller <dsmiller95@gmail.com>
2022-04-17 16:44:50 -0500

    add context-sensitive startup scripts to variable formatters

--- Assets/UtilityScripts/com.dman.reactive-variables/Runtime/VariableOperators/FloatVariableFormatter.cs
+++ Assets/UtilityScripts/com.dman.reactive-variables/Runtime/VariableOperators/FloatVariableFormatter.cs
@@ -1,4 +1,6 @@
-﻿using UniRx;
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using UniRx;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
@@ -27,5 +29,39 @@ namespace Dman.ReactiveVariables.VariableOperators
             var newText = string.Format(formatString, value);
             TextUpdate?.Invoke(newText);
         }
+
+#if UNITY_EDITOR
+        private void Reset()
+        {
+            TextUpdate = new UnityEvent<string>();
+            var allComponents = gameObject.GetComponents(typeof(MonoBehaviour));
+            foreach (var comp in allComponents)
+            {
+                var componentType = comp.GetType();
+                var componentTextProperty = componentType.GetProperties()
+                    .Where(x => x.PropertyType == typeof(string) && x.Name == "text" && x.CanWrite && x.SetMethod.IsPublic)
+                    .FirstOrDefault();
+
+                if (componentTextProperty == null)
+                    continue;
+
+
+                var textSetMethod = componentTextProperty.GetSetMethod().CreateDelegate(typeof(UnityAction<string>), comp) as UnityAction<string>;
+
+                UnityEditor.Events.UnityEventTools.AddPersistentListener<string>(
+                    TextUpdate,
+                    textSetMethod);
+
+                var existingText = componentTextProperty.GetMethod.Invoke(comp, new object[0]) as string;
+                var numberMatch = new Regex(@"[-\d.]+").Match(existingText);
+                if (numberMatch != null)
+                {
+                    var myText = existingText.Substring(0, numberMatch.Index) + "{0,6:F1}" + existingText.Substring(numberMatch.Index + numberMatch.Length);
+                    formatString = myText;
+                }
+                break;
+            }
+        }
+#endif
     }
 }
--- Assets/UtilityScripts/com.dman.reactive-variables/Runtime/VariableOperators/IntVariableFormatter.cs
+++ Assets/UtilityScripts/com.dman.reactive-variables/Runtime/VariableOperators/IntVariableFormatter.cs
@@ -2,6 +2,8 @@
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Dman.ReactiveVariables.VariableOperators
 {
@@ -22,5 +24,39 @@ namespace Dman.ReactiveVariables.VariableOperators
                     TextUpdate?.Invoke(newText);
                 }).AddTo(this);
         }
+
+#if UNITY_EDITOR
+        private void Reset()
+        {
+            TextUpdate = new UnityEvent<string>();
+            var allComponents = gameObject.GetComponents(typeof(MonoBehaviour));
+            foreach (var comp in allComponents)
+            {
+                var componentType = comp.GetType();
+                var componentTextProperty = componentType.GetProperties()
+                    .Where(x => x.PropertyType == typeof(string) && x.Name == "text" && x.CanWrite && x.SetMethod.IsPublic && x.CanRead)
+                    .FirstOrDefault();
+
+                if (componentTextProperty == null)
+                    continue;
+
+
+                var textSetMethod = componentTextProperty.SetMethod.CreateDelegate(typeof(UnityAction<string>), comp) as UnityAction<string>;
+
+                UnityEditor.Events.UnityEventTools.AddPersistentListener<string>(
+                    TextUpdate,
+                    textSetMethod);
+
+                var existingText = componentTextProperty.GetMethod.Invoke(comp, new object[0]) as string;
+                var numberMatch = new Regex(@"\d+").Match(existingText);
+                if(numberMatch != null)
+                {
+                    var myText = existingText.Substring(0, numberMatch.Index) + "{0,3:D}" + existingText.Substring(numberMatch.Index + numberMatch.Length);
+                    formatString = myText;
+                }
+                break;
+            }
+        }
+#endif
     }
 }
