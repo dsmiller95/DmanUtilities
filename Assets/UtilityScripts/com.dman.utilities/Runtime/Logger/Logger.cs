8390f85668bb0f0eeef42edffaefee2a27ee657e
Dan Miller <dsmiller95@gmail.com>
2023-01-08 22:53:45 -0600

    add tiny logger util

---
+++ Assets/UtilityScripts/com.dman.utilities/Runtime/Logger/Logger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Dman.Utilities
+{
+    public static class Logger
+    {
+        public static void Log(this MonoBehaviour sourceScope, string log)
+        {
+            var sourceType = sourceScope.GetType();
+            Debug.Log(sourceType.Name + ": " + log, sourceScope);
+        }
+    }
+}
