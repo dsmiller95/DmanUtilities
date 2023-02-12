4c7e1a557717eac4d5f46d578c67afa490f27e34
Dan Miller <dsmiller95@gmail.com>
2023-02-12 11:22:20 -0600

    modify the scoped stable id to actually strore registered objects as well. it becomes a scope provider and a singleton object registry

--- Assets/UtilityScripts/com.dman.utilities/Runtime/ScopedStableIdProvider.cs
+++ Assets/UtilityScripts/com.dman.utilities/Runtime/ScopedStableIdProvider.cs
@@ -21,55 +21,80 @@ namespace Dman.NarrativeSystem
         }
 
 
-        private HashSet<string> usedIds = new HashSet<string>();
+        private Dictionary<string, T> registeredObjects = new Dictionary<string, T>();
         private Stack<string> scopeStack = new Stack<string>();
 
-        private ScopedStableIdProvider()
-        {
+        private ScopedStableIdProvider() { }
 
-        }
-
-        public static string GetStableId(string localId)
+        /// <summary>
+        /// Register <paramref name="registered"/> object with a given local id, with the stable scoped id
+        /// </summary>
+        /// <param name="localId">the local ID to register against</param>
+        /// <param name="registered">the registered object</param>
+        /// <returns>the new stable ID which was registered against</returns>
+        public static string RegeisterOnStableId(string localId, T registered)
         {
-            return Instance.GetStableIdInternal(localId);
-        }
-        public static IDisposable PushScope(string localId)
-        {
-            return Instance.PushScopeInternal(localId);
-        }
-        public static void ClearAll()
-        {
-            Instance.ClearAllInternal();
+            return Instance.RegeisterOnStableIdInternal(localId, registered);
         }
 
-        private string GetStableIdInternal(string localIdentifier)
+        private string RegeisterOnStableIdInternal(string localIdentifier, T registered)
         {
             var baseScope = string.Join(".", scopeStack);
             var newId = baseScope + "." + localIdentifier;
             Debug.Log($"adding a {typeof(T).Name} with id: {newId}");
-            if (!usedIds.Add(newId))
+            if (registeredObjects.ContainsKey(newId))
             {
                 Debug.LogError($"adding a {typeof(T).Name} with an id which has already been used: {newId}");
             }
+            registeredObjects[newId] = registered;
             return newId;
         }
 
-        private IDisposable PushScopeInternal(string scope)
+        public static IEnumerable<T> GetAll()
         {
-            scopeStack.Push(scope);
-            return new DisposableAbuse.LambdaDispose(() => PopScope(scope));
+            return Instance.GetAllInternal();
+        }
+        private IEnumerable<T> GetAllInternal()
+        {
+            return registeredObjects.Values;
+        }
+        public static T Get(string stableId)
+        {
+            return Instance.GetInternal(stableId);
+        }
+        private T GetInternal(string stableId)
+        {
+            if(!registeredObjects.TryGetValue(stableId, out var result))
+            {
+                throw new Exception($"ScopedStableIdProvider.Get: Tried to get object from missing id: '{stableId}'");
+            }
+            return result;
         }
 
+        public static void ClearAll()
+        {
+            Instance.ClearAllInternal();
+        }
         private void ClearAllInternal()
         {
-            usedIds.Clear();
-            if(scopeStack.Count > 0)
+            registeredObjects.Clear();
+            if (scopeStack.Count > 0)
             {
                 Debug.LogError($"clearing stable id stack of {typeof(T).Name} when scopes are currently stacked. This will lead to errors");
                 scopeStack.Clear();
             }
         }
 
+
+        public static IDisposable PushScope(string localId)
+        {
+            return Instance.PushScopeInternal(localId);
+        }
+        private IDisposable PushScopeInternal(string scope)
+        {
+            scopeStack.Push(scope);
+            return new DisposableAbuse.LambdaDispose(() => PopScope(scope));
+        }
         private void PopScope(string scope)
         {
             if (scopeStack.Peek() != scope)
