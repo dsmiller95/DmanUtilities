5cc02ec031bbf936ab4a2142ea13a1ba2453e2df
Dan Miller <dsmiller95@gmail.com>
2023-03-25 15:10:00 -0500

    add an option to "toggle" a scriptable object variable

--- Assets/UtilityScripts/com.dman.reactive-variables/Runtime/ScriptableObjectVariable.cs
+++ Assets/UtilityScripts/com.dman.reactive-variables/Runtime/ScriptableObjectVariable.cs
@@ -5,5 +5,21 @@ namespace Dman.ReactiveVariables
     [CreateAssetMenu(fileName = "ScriptableObjectVariable", menuName = "DmanUtilities/ReactiveVariables/ScriptableObjectVariable", order = 1)]
     public class ScriptableObjectVariable : GenericVariable<ScriptableObject>
     {
+        /// <summary>
+        /// Set the value of this variable to <paramref name="value"/>. If it is already set to the same value,
+        /// Reset to null.
+        /// </summary>
+        /// <param name="value"></param>
+        public void ToggleValue(ScriptableObject value)
+        {
+            if (CurrentValue == value)
+            {
+                ClearToDefaultValue();
+            }
+            else
+            {
+                SetValue(value);
+            }
+        }
     }
 }
