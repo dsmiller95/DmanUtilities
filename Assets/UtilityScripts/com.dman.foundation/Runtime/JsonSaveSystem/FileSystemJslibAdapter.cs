6b06debc9457fe04c0ee2fd304d3a70458fba894
Dan Miller <dan.miller@fraculation.com>
2024-08-29 10:40:40 -0500

    0.1.4 bump ver fix namespace import error

--- Assets/UtilityScripts/com.dman.foundation/Runtime/JsonSaveSystem/FileSystemJslibAdapter.cs
+++ Assets/UtilityScripts/com.dman.foundation/Runtime/JsonSaveSystem/FileSystemJslibAdapter.cs
@@ -1,11 +1,9 @@
-﻿using System.Runtime.CompilerServices;
-
-namespace Dman.SaveSystem
+﻿namespace Dman.SaveSystem
 {
     public static class FileSystemJslibAdapter
     {
 #if UNITY_WEBGL && !UNITY_EDITOR
-        [DllImport("__Internal")]
+        [System.Runtime.InteropServices.DllImport("__Internal")]
         private static extern void JS_FileSystem_Sync();
 #else
         private static void JS_FileSystem_Sync() { }
