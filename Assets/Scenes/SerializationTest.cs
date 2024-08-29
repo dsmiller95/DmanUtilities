fc536965af1c57b7d0ed02775664383cf69ff907
Dan M <dsmiller95@gmail.com>
2024-08-29 14:31:22 -0500

    add a script to help visualize how unity serializes certain primitives

---
+++ Assets/Scenes/SerializationTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+#pragma warning disable CS0169 // Field is never used
+
+
+public class SerializationTest : MonoBehaviour
+{
+    [SerializeField] private Vector2 testVector2;
+    [SerializeField] private Vector3 testVector3;
+    [SerializeField] private Vector4 testVector4;
+    [SerializeField] private Quaternion testQuaternion;
+    [SerializeField] private Matrix4x4 testMatrix4x4;
+    [SerializeField] private Color testColor;
+    [SerializeField] private Rect testRect;
+    [SerializeField] private LayerMask testLayerMask;
+    [SerializeField] private AnimationCurve testAnimationCurve;
+}
