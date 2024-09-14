eb8f386ed11018b8dc3ac70f7c14721034734e06
Dan Miller <dan.miller@fraculation.com>
2024-09-14 11:46:35 -0500

    add shared lock

---
+++ Assets/UtilityScripts/com.dman.utilities/Runtime/SharedLock.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Dman.Utilities
+{
+    public class SharedLock
+    {
+        public bool IsLocked => TotalLocks > 0;
+        public int TotalLocks { get; private set; }
+
+        private class Lock : IDisposable
+        {
+            private SharedLock resource;
+            private bool isDisposed = false;
+
+            public Lock(SharedLock resource)
+            {
+                this.resource = resource;
+                this.resource.TotalLocks++;
+            }
+
+            private void ReleaseUnmanagedResources()
+            {
+                if (isDisposed) return;
+                isDisposed = true;
+                this.resource.TotalLocks--;
+            }
+
+            public void Dispose()
+            {
+                ReleaseUnmanagedResources();
+                GC.SuppressFinalize(this);
+            }
+
+            ~Lock() { ReleaseUnmanagedResources(); }
+        }
+
+        public IDisposable TakeLock()
+        {
+            return new Lock(this);
+        }
+    }
+}
